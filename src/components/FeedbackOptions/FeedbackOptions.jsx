9a237456af6c6cfdc434be548e440f1a1b18fb63
YurecUA <81627173+YurecUA@users.noreply.github.com>
2022-12-29 17:24:10 +0200

    Create FeedbackOptions.jsx

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,30 @@
+import styles from './FeedbackOptions.module.css';
+import PropTypes from 'prop-types';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <>
+      <div>
+        {options.map(option => {
+          return (
+            <button
+              type="button"
+              onClick={() => onLeaveFeedback(option)}
+              key={option}
+              className={styles.btn}
+            >
+              {option}
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
